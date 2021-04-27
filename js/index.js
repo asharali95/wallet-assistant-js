fca573ff2a540dc325eb353e62039848e81ad437
Ashar Ali <ashar.alia1999@gmail.com>
2021-04-27 19:53:07 +0500

    few design changes

--- js/index.js
+++ js/index.js
@@ -8,13 +8,12 @@
 */
 // global variables:
 //----------------------------------
-var auth = firebase.auth()
-var firestore = firebase.firestore()
+var auth = firebase.auth();
+var firestore = firebase.firestore();
 var signinForm = document.querySelector(".signinForm");
 var signupForm = document.querySelector(".signupForm");
 var googleSignupButton = document.querySelector(".googleSignup");
 //----------------------------------
-
 
 //global function:
 //----------------------------------
@@ -29,31 +28,33 @@ var googleSignupButton = document.querySelector(".googleSignup");
   vi. redirect to dashboard page
 */
 var signupFormSubmission = async (e) => {
-    e.preventDefault();
-    try{
-    var fullName = document.querySelector(".signupFullName").value
-    var email = document.querySelector(".signupEmail").value
-    var password = document.querySelector(".signupPassword").value
-    if(fullName && email && password){ //empty string false hoti hamesha
-        //create user in auth section
-        var {user:{uid}} = await auth.createUserWithEmailAndPassword(email,password) 
+  e.preventDefault();
+  try {
+    var fullName = document.querySelector(".signupFullName").value;
+    var email = document.querySelector(".signupEmail").value;
+    var password = document.querySelector(".signupPassword").value;
+    if (fullName && email && password) {
+      //empty string false hoti hamesha
+      //create user in auth section
+      var {
+        user: { uid },
+      } = await auth.createUserWithEmailAndPassword(email, password);
 
-        //store user data in firestore
-            var userInfo = {
-                fullName,
-                email,
-                createdAt: new Date()
-            }
-           
-         await firestore.collection("Users").doc(uid).set(userInfo)
-         console.log("sign up successful")
-         signupForm.reset()
-        }
+      //store user data in firestore
+      var userInfo = {
+        fullName,
+        email,
+        createdAt: new Date(),
+      };
+
+      await firestore.collection("Users").doc(uid).set(userInfo);
+      console.log("sign up successful");
+      signupForm.reset();
     }
-    catch(error){
-        console.log(error)
-    }
-} 
+  } catch (error) {
+    console.log(error);
+  }
+};
 /* 
  steps I did in this function
   i.   collect form values
@@ -61,19 +62,21 @@ var signupFormSubmission = async (e) => {
   iii. redirect to dashboard along with uid 
 */
 var signinFormSubmission = async (e) => {
-    e.preventDefault();
-    try {
-        var email = document.querySelector(".signinEmail").value
-        var password = document.querySelector(".signinPassword").value
-        if(email && password){
-            // login user
-        var {user: {uid}} = await auth.signInWithEmailAndPassword(email,password)
-        location.assign(`./dashboard.html#${uid}`)
-        }
-    } catch (error) {
-        console.log(error)
+  e.preventDefault();
+  try {
+    var email = document.querySelector(".signinEmail").value;
+    var password = document.querySelector(".signinPassword").value;
+    if (email && password) {
+      // login user
+      var {
+        user: { uid },
+      } = await auth.signInWithEmailAndPassword(email, password);
+      location.assign(`./dashboard.html#${uid}`);
     }
- } 
+  } catch (error) {
+    console.log(error);
+  }
+};
 /* 
  steps I did in this function:
 
@@ -86,33 +89,39 @@ var signinFormSubmission = async (e) => {
         if old user:
            . redirect to dashboard with uid 
 */
-var googleSignin = async() =>{
-   try {
+var googleSignin = async () => {
+  try {
     var googleProvider = new firebase.auth.GoogleAuthProvider();
-    var {additionalUserInfo:{isNewUser},user:{displayName,uid,email}}  = await auth.signInWithPopup(googleProvider)
-
-   if(isNewUser){
-    var userInfo = {
-        fullName : displayName,
+    googleProvider.setCustomParameters({
+        prompt: "select_account",
+      });
+    var {
+      additionalUserInfo: { isNewUser },
+      user: { displayName, uid, email },
+    } = await auth.signInWithPopup(googleProvider);
+    
+    if (isNewUser) {
+      var userInfo = {
+        fullName: displayName,
         email,
-        createdAt: new Date()
+        createdAt: new Date(),
+      };
+      // console.log(userInfo)
+      await firestore.collection("Users").doc(uid).set(userInfo);
+      location.assign(`./dashboard.html#${uid}`);
     }
-    // console.log(userInfo)
-    await firestore.collection("Users").doc(uid).set(userInfo);
-    location.assign(`./dashboard.html#${uid}`)
-   } 
-   location.assign(`./dashboard.html#${uid}`)
-   } catch (error) {
-    console.log(error)
-   }
-}
+    location.assign(`./dashboard.html#${uid}`);
+  } catch (error) {
+    console.log(error);
+  }
+};
 //----------------------------------
 
-// all event Listeners: 
+// all event Listeners:
 //----------------------------------
-signinForm.addEventListener("submit" ,(e)=>signinFormSubmission(e));
-signupForm.addEventListener("submit" ,(e)=>signupFormSubmission(e));
-googleSignupButton.addEventListener("click",googleSignin);
+signinForm.addEventListener("submit", (e) => signinFormSubmission(e));
+signupForm.addEventListener("submit", (e) => signupFormSubmission(e));
+googleSignupButton.addEventListener("click", googleSignin);
 
 // auth.onAuthStateChanged(async(user) =>{
 //     if(user){
--- js/transaction.js
+++ js/transaction.js
@@ -6,64 +6,63 @@ var transactionTitle = document.querySelector(".title");
 var transactionType = document.querySelector(".transaction-type");
 var transactionAt = document.querySelector(".transactionAt");
 var transactionId = location.hash.substring(1, location.hash.length);
-var transactionForm = document.querySelector(".transactionForm")
+var transactionForm = document.querySelector(".transactionForm");
 var deleteBtn = document.querySelector(".deleteBtn");
 
 var fetchSpecificTransaction = async (transactionId) => {
   try {
-   //fetch transaction by it's ID
-  var transaction = await firestore
-  .collection("transactions")
-  .doc(transactionId)
-  .get();
-  return transaction.data();
-   
+    //fetch transaction by it's ID
+    var transaction = await firestore
+      .collection("transactions")
+      .doc(transactionId)
+      .get();
+    return transaction.data();
+  } catch (error) {}
+};
+
+var editFormHandler = async (e, transactionId) => {
+  try {
+    e.preventDefault();
+    var updatedTitle = transactionTitle.value;
+    var updatedCost = transactionCost.value;
+    var updatedTransType = transactionType.value;
+    var updatedTransAt = transactionAt.value;
+    var updatedTransactionObj = {
+      title: updatedTitle,
+      cost: parseInt(updatedCost),
+      transactionType: updatedTransType,
+      transactionAt: new Date(updatedTransAt),
+    };
+    await firestore
+      .collection("transactions")
+      .doc(transactionId)
+      .update(updatedTransactionObj);
+    location.assign(`./dashboard.html`);
   } catch (error) {
-      
+    console.log(error);
   }
 };
 
-var editFormHandler = async (e, transactionId,uid) =>{
-    try {
-        e.preventDefault()
-        var updatedTitle =transactionTitle.value
-        var updatedCost = transactionCost.value
-        var updatedTransType = transactionType.value
-        var updatedTransAt = transactionAt.value
-        var updatedTransactionObj = {
-            title: updatedTitle,
-            cost: parseInt(updatedCost),
-            transactionType:updatedTransType ,
-            transactionAt: new Date(updatedTransAt)
-        }
-        await firestore
-        .collection("transactions")
-        .doc(transactionId)
-        .update(updatedTransactionObj)
-        location.assign(`./dashboard.html`)
-    } catch (error) {
-        console.log(error)
-    } 
-}
+var deleteTransaction = async (transactionId) => {
+  try {
+    await firestore
+      .collection("transactions")
+      .doc(transactionId)
+      .delete()
+      .then(() => {
+        location.assign(`./dashboard.html`);
+      });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-var deleteTransaction = async (transactionId) =>{
-   try {
-     await firestore
-    .collection("transactions")
-    .doc(transactionId)
-    .delete().then(() =>{
-        location.assign(`./dashboard.html`)
-    })    
-   } catch (error) {
-       console.log(error)
-   }
-    
-}
-
-deleteBtn.addEventListener("click",() => deleteTransaction(transactionId))
+deleteBtn.addEventListener("click", () => deleteTransaction(transactionId));
 // event listeners
 // -auth listener
-transactionForm.addEventListener("submit",(e) => editFormHandler(e,transactionId))
+transactionForm.addEventListener("submit", (e) =>
+  editFormHandler(e, transactionId)
+);
 auth.onAuthStateChanged(async (user) => {
   if (user) {
     //form initial value handling
@@ -74,7 +73,7 @@ auth.onAuthStateChanged(async (user) => {
       transactionAt: transAt,
     } = await fetchSpecificTransaction(transactionId);
 
-   //setting initial value:
+    //setting initial value:
     transactionTitle.value = title;
     transactionCost.value = cost;
     transactionType.value = transType;
